9bf2eecf54a26e46835665e62938b22fa39bdd5b
JosepharDev <josephardev@gmail.com>
2025-06-14 15:33:43 +0100

    add p1

---
+++ p1/server_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#install k3s
+
+curl -sfL https://get.k3s.io | sh -
+
+sudo mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
+
+TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+
+echo $TOKEN > /vagrant/token
